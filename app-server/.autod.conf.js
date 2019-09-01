bf6dce220b225a1618e3f4f820d70b0d03b32be1
padingdun <padingdun@pinduoduo.com>
2019-09-01 23:34:36 +0800

    padingdun feature commit

--- app-server/.autod.conf.js
+++ app-server/.autod.conf.js
@@ -1,30 +1,20 @@
-'use strict';
+"use strict";
 
 module.exports = {
   write: true,
-  prefix: '^',
-  plugin: 'autod-egg',
-  test: [
-    'test',
-    'benchmark',
-  ],
-  dep: [
-    'egg',
-    'egg-scripts',
-  ],
+  prefix: "^",
+  plugin: "autod-egg",
+  test: ["test", "benchmark"],
+  dep: ["egg", "egg-scripts"],
   devdep: [
-    'egg-ci',
-    'egg-bin',
-    'egg-mock',
-    'autod',
-    'autod-egg',
-    'eslint',
-    'eslint-config-egg',
-    'webstorm-disable-index',
+    "egg-ci",
+    "egg-bin",
+    "egg-mock",
+    "autod",
+    "autod-egg",
+    "eslint",
+    "eslint-config-egg",
+    "webstorm-disable-index"
   ],
-  exclude: [
-    './test/fixtures',
-    './dist',
-  ],
+  exclude: ["./test/fixtures", "./dist"]
 };
-
